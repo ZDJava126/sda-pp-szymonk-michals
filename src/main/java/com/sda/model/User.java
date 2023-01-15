85966fe61d04d77dc8c196adbb365892fe4b38cd
Veirox-sim <szymonfbkosowski@gmail.com>
2023-01-15 12:47:54 +0100

    HibernateUtils modfied, User class added

---
+++ src/main/java/com/sda/model/User.java
@@ -0,0 +1,43 @@
+package com.sda.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+
+        User user = (User) o;
+        return username != null && Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
